72fea382673e6f4031bfbe665aefd6f59b37ffe4
Ahmad Al-Jabbouri <alja0062@algonquinlive.com>
2023-08-06 00:44:33 -0400

    done with program-1; in short, created a module which converts stufile.txt to stufile.dat + stufile.dat.1

--- src/STUDENT-REPORT.cbl
+++ src/STUDENT-REPORT.cbl
@@ -1,25 +1,30 @@
 ******************************************************************
       * Author:    Stefan Stivicic
       *            Andre Azevedo de Rocha
-      * Date: July 14th
+      *            Ahmad Al-Jabbouri
+      * Date: 2023 August 5th
       * Purpose:
       * Tectonics: cobc
       ******************************************************************
        IDENTIFICATION DIVISION.
        PROGRAM-ID. STUDENT-REPORT.
+
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
-            SELECT PROGRAM-FILE ASSIGN TO
-            "../PROGRAM.txt"
+      *    List of programs.
+            SELECT PROGRAM-FILE ASSIGN TO "../PROGRAM.txt"
                 ORGANIZATION IS LINE SEQUENTIAL.
-            SELECT STUDENT-FILE ASSIGN TO
-            "../STUFILE.txt"
+      *    Raw input data to be processed.
+            SELECT STUDENT-FILE ASSIGN TO "../STUFILE.txt"
                 ORGANIZATION IS LINE SEQUENTIAL.
-            SELECT OUTPUT-FILE ASSIGN TO "../OUTPUT.txt"
+      *    Processed output student report.
+            SELECT OUTPUT-FILE  ASSIGN TO "../OUTPUT.txt"
                 ORGANIZATION IS LINE SEQUENTIAL.
+
        DATA DIVISION.
        FILE SECTION.
+
        FD PROGRAM-FILE.
        01 PROGRAM-RECORD.
            05 PROGRAM-CODE     PIC X(6).
@@ -53,6 +58,11 @@
            05 TUITION-OWED-OUT PIC Z,ZZ9.99.
 
        WORKING-STORAGE SECTION.
+
+       01 MODULES.
+           05 TEST-MODULE PIC X(13) VALUE "TEST-MODULE".
+           05 FILE-CONVERTER-MODULE PIC X(50) VALUE "MODULE-1".
+
        01 PROGRAM-TABLE.
            05 PROGRAM-TABLE-ENTRY OCCURS 20 TIMES INDEXED T-ENTRY.
                10 PROGRAM-TABLE-CODE PIC X(6).
@@ -69,6 +79,8 @@
 
        01 TEMP-VALUES.
            05 STUDENT-AVG PIC 9(3)V9.
+           05 CACHE       PIC X(108).
+           05 FILE-STATUS PIC X(2).
 
        01 COUNTERS.
            05 READ-COUNTER PIC 99.
@@ -82,6 +94,7 @@
 
        PROCEDURE DIVISION.
        100-MAIN.
+           CALL TEST-MODULE USING STUDENT-RECORD.
            PERFORM 201-OPEN-FILES.
            PERFORM 202-GENERATE-RECORDS.
            PERFORM 203-CLOSE-FILES.
@@ -95,19 +108,9 @@
        202-GENERATE-RECORDS.
            PERFORM 301-GENERATE-HEADER.
            PERFORM 302-READ-PROGRAM-TABLE.
-           PERFORM UNTIL EOF-STU = 'Y'
-               READ STUDENT-FILE
-               AT END
-                       MOVE 'Y' TO EOF-STU
-               NOT AT END
-                   ADD 1 TO READ-COUNTER
-                   PERFORM 303-WRITE-STUDENT
-                   ADD 1 TO WRITE-COUNTER
-               END-READ
-           END-PERFORM.
+           PERFORM 304-READ-STUDENT-FILE UNTIL EOF-STU = 'Y'.
            DISPLAY "READ COUNT: " READ-COUNTER.
            DISPLAY "WRITE COUNT: " WRITE-COUNTER.
-
 
        203-CLOSE-FILES.
            CLOSE PROGRAM-FILE
@@ -130,14 +133,29 @@
            PERFORM 403-SEARCH.
            WRITE STUDENT-OUTPUT-FILE.
 
+       304-READ-STUDENT-FILE.
+           READ STUDENT-FILE
+               AT END
+                       MOVE 'Y' TO EOF-STU
+               NOT AT END
+                   CALL FILE-CONVERTER-MODULE USING STUDENT-RECORD
+                   ADD 1 TO READ-COUNTER
+                   PERFORM 303-WRITE-STUDENT
+                   ADD 1 TO WRITE-COUNTER
+           END-READ.
+
 
        401-LOAD-PROGRAM-TABLE.
+      *     DISPLAY " PREREAD PROGRAM-NAME = " PROGRAM-NAME.
            READ PROGRAM-FILE
            AT END
                MOVE 'Y' TO EOF-PROG
            NOT AT END
+      *     DISPLAY "POSTREAD PROGRAM-NAME = " PROGRAM-NAME
+      *    This test shows READ statement already knows where to fit the record segments; just describe the file, then read
                MOVE PROGRAM-RECORD TO PROGRAM-TABLE-ENTRY(T-ENTRY)
                ADD 1 TO T-ENTRY.
+      *     DISPLAY "AT 401: " CACHE.
 
        402-GET-STUDENT-AVERAGE.
            MOVE 0 TO STUDENT-AVG.
