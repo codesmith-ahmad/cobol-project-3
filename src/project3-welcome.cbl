ec930a5a4552181d9b6fe00f9e638d576cb331a9
SavindyaSomakeerthi <119979099+SavindyaSomakeerthi@users.noreply.github.com>
2023-08-12 23:51:17 -0400

    Add files via upload

--- src/project3-welcome.cbl
+++ src/project3-welcome.cbl
@@ -39,6 +39,7 @@
        01 EOF-FLAG PIC X VALUE 'N'.
        01 STUDENT-INPUT.
            05 WS-OPTION PIC 9.
+           05 PAY-IN PIC 9(4)V99.
 
        01 FOUND-STUDENT-RECORD.
            05 FOUND-STUDENT-NUMBER PIC 9(6).
@@ -66,62 +67,107 @@
                40.
            05 VALUE "*******************************************"
                LINE 4 COL 30.
-           05 VALUE "ENTER THE KEY: " LINE 6 COL 10.
-           05 STUDENT-ID-IN LINE 6 COL 25 PIC 9(6) TO USER-KEY.
+           05 VALUE "WHAT YOU WANT TO DO?"  LINE 6 COL 10.
+           05 VALUE "1. SEARCH ANOTHER STUDENT [S] OR [1] "  LINE 7
+               COL 10.
+           05 VALUE "2. ENTER PAYMENT [P] OR [2] " LINE 8
+               COL 10.
+           05 VALUE "3. GENERATE REPORT [R] OR [3] " LINE 9
+               COL 10.
+           05 VALUE "3. QUIT [Q] OR [4]" LINE 10 COL 10.
+           05 VALUE "ENTER THE OPTION: " LINE 12 COL 10.
+           05 OPTION-IN LINE 12 COL 27 PIC 9 TO WS-OPTION.
 
-       01 OUTPUT-SCREEN.
+       01 SEARCH-SCREEN.
            05 VALUE "*******************************************"
                BLANK SCREEN LINE 1 COL 30.
-           05 VALUE "Details of Student" LINE 2 COL 45.
+           05 VALUE "DETAILS OF STUDENT" LINE 2 COL 45.
            05 VALUE "*******************************************"
                LINE 3 COL 30.
-           05 VALUE "STUDENT NAME :" LINE 4 COL 10.
-           05 STUDENT-NAME-IN LINE 4 COL 25 PIC X(40)
-               FROM FOUND-STUDENT-NAME.
-           05 VALUE "TUTION OWED :" LINE 5 COL 10.
-           05 TUITION-OWED-IN LINE 5 COL 25 PIC 9(4).99
-               FROM FOUND-TUITION-OWED.
-           05 VALUE "PROGRAM NAME :" LINE 6 COL 10.
-           05 PROGRAM-OF-STUDY-IN LINE 6 COL 25 PIC X(6)
-               FROM FOUND-PROGRAM-OF-STUDY.
-           05 VALUE "COURSES :" LINE 8 COL 10.
-           05 VALUE "COURSES CODE " LINE 10 COL 25.
-           05 VALUE "COURSES AVG " LINE 10 COL 48.
-           05 VALUE "----------------------------------" LINE 11 COL 25.
-           05 COURSE-CODE-1-IN LINE 12 COL 25 PIC X(7)
-               FROM FOUND-COURSE-CODE-1.
-           05 COURSE-AVERAGE-1-IN LINE 12 COL 48 PIC 9(3)
-               FROM FOUND-COURSE-AVERAGE-1.
-           05 COURSE-CODE-2-IN LINE 12 COL 25 PIC X(7)
-               FROM FOUND-COURSE-CODE-2.
-           05 COURSE-AVERAGE-2-IN LINE 12 COL 48 PIC 9(3)
-               FROM FOUND-COURSE-AVERAGE-2.
-           05 COURSE-CODE-3-IN LINE 12 COL 25 PIC X(7)
-               FROM FOUND-COURSE-CODE-3.
-           05 COURSE-AVERAGE-3-IN LINE 12 COL 48 PIC 9(3)
-               FROM FOUND-COURSE-AVERAGE-3.
-           05 COURSE-CODE-4-IN LINE 12 COL 25 PIC X(7)
-               FROM FOUND-COURSE-CODE-4.
-           05 COURSE-AVERAGE-4-IN LINE 12 COL 48 PIC 9(3)
-               FROM FOUND-COURSE-AVERAGE-4.
-           05 COURSE-CODE-5-IN LINE 12 COL 25 PIC X(7)
-               FROM FOUND-COURSE-CODE-5.
-           05 COURSE-AVERAGE-5-IN LINE 12 COL 48 PIC 9(3)
-               FROM FOUND-COURSE-AVERAGE-5.
-           05 VALUE "----------------------------------" LINE 13 COL 25.
-      *>      need to put the total value after compute
-           05 TOTAL-IN LINE 14 COL 25.
-      *>      need to put the total avg value after compute
-           05 TOTAL-AVG-IN LINE 14 COL 48.
+           05 VALUE "ENTER THE KEY: " LINE 4 COL 10.
+           05 STUDENT-ID-IN LINE 4 COL 25 PIC 9(6) TO USER-KEY.
 
-           05 VALUE "WHAT YOU WANT TO DO?"  LINE 16 COL 10.
-           05 VALUE "1. SEARCH ANOTHER STUDENT [S] OR [1] "  LINE 17
+       01 OUTPUT-SCREEN.
+           05 VALUE "STUDENT NAME :" LINE 6 COL 10.
+           05 STUDENT-NAME-IN LINE 6 COL 25 PIC X(40)
+               FROM FOUND-STUDENT-NAME.
+           05 VALUE "TUTION OWED :" LINE 7 COL 10.
+           05 TUITION-OWED-IN LINE 7 COL 25 PIC 9(4).99
+               FROM FOUND-TUITION-OWED.
+           05 VALUE "PROGRAM NAME :" LINE 8 COL 10.
+           05 PROGRAM-OF-STUDY-IN LINE 8 COL 25 PIC X(6)
+               FROM FOUND-PROGRAM-OF-STUDY.
+           05 VALUE "COURSES :" LINE 10 COL 10.
+           05 VALUE "COURSES CODE " LINE 12 COL 25.
+           05 VALUE "COURSES AVG " LINE 12 COL 48.
+           05 VALUE "----------------------------------" LINE 13 COL 25.
+           05 COURSE-CODE-1-IN LINE 14 COL 25 PIC X(7)
+               FROM FOUND-COURSE-CODE-1.
+           05 COURSE-AVERAGE-1-IN LINE 14 COL 48 PIC 9(3)
+               FROM FOUND-COURSE-AVERAGE-1.
+           05 COURSE-CODE-2-IN LINE 15 COL 25 PIC X(7)
+               FROM FOUND-COURSE-CODE-2.
+           05 COURSE-AVERAGE-2-IN LINE 15 COL 48 PIC 9(3)
+               FROM FOUND-COURSE-AVERAGE-2.
+           05 COURSE-CODE-3-IN LINE 16 COL 25 PIC X(7)
+               FROM FOUND-COURSE-CODE-3.
+           05 COURSE-AVERAGE-3-IN LINE 16 COL 48 PIC 9(3)
+               FROM FOUND-COURSE-AVERAGE-3.
+           05 COURSE-CODE-4-IN LINE 17 COL 25 PIC X(7)
+               FROM FOUND-COURSE-CODE-4.
+           05 COURSE-AVERAGE-4-IN LINE 17 COL 48 PIC 9(3)
+               FROM FOUND-COURSE-AVERAGE-4.
+           05 COURSE-CODE-5-IN LINE 18 COL 25 PIC X(7)
+               FROM FOUND-COURSE-CODE-5.
+           05 COURSE-AVERAGE-5-IN LINE 18 COL 48 PIC 9(3)
+               FROM FOUND-COURSE-AVERAGE-5.
+           05 VALUE "----------------------------------" LINE 19 COL 25.
+      *>      need to put the total value after compute
+           05 TOTAL-IN LINE 20 COL 25.
+      *>      need to put the total avg value after compute
+           05 TOTAL-AVG-IN LINE 20 COL 48.
+
+           05 VALUE "WHAT YOU WANT TO DO?"  LINE 22 COL 10.
+           05 VALUE "1. SEARCH ANOTHER STUDENT [S] OR [1] "  LINE 24
                COL 10.
-           05 VALUE "2. EDIT STUDENT DETAILS [P] OR [2] " LINE 18
+           05 VALUE "2. ENTER PAYMENT [P] OR [2] " LINE 25
                COL 10.
-           05 VALUE "3. QUIT [Q] OR [3]" LINE 19 COL 10.
-           05 VALUE "ENTER THE OPTION: " LINE 21 COL 10.
-           05 OPTION-IN LINE 21 COL 27 PIC 9 TO WS-OPTION.
+           05 VALUE "3. GENERATE REPORT [R] OR [3] " LINE 26
+               COL 10.
+           05 VALUE "3. QUIT [Q] OR [4]" LINE 27 COL 10.
+           05 VALUE "ENTER THE OPTION: " LINE 28 COL 10.
+           05 OPTION-IN LINE 28 COL 27 PIC 9 TO WS-OPTION.
+
+       01 UPDATE-SCREEN.
+           05 VALUE "*******************************************"
+               BLANK SCREEN LINE 1 COL 30.
+           05 VALUE "UPDATE THE PAYMENT" LINE 2 COL 45.
+           05 VALUE "*******************************************"
+               LINE 3 COL 30.
+           05 VALUE "ENTER THE PAYMENT: " LINE 4 COL 10.
+           05 TUTION-PAY-IN LINE 4 COL 30 PIC 9(6) TO PAY-IN.
+
+       01 UPDATE-OUT-SCREEN.
+           05 VALUE "STUDENT NAME :" LINE 6 COL 10.
+           05 STUDENT-NAME-IN LINE 6 COL 25 PIC X(40)
+               FROM FOUND-STUDENT-NAME.
+           05 VALUE "TUTION OWED :" LINE 7 COL 10.
+           05 TUITION-OWED-IN LINE 7 COL 25 PIC 9(4).99
+               FROM FOUND-TUITION-OWED.
+           05 VALUE "PROGRAM NAME :" LINE 8 COL 10.
+           05 PROGRAM-OF-STUDY-IN LINE 8 COL 25 PIC X(6)
+               FROM FOUND-PROGRAM-OF-STUDY.
+
+           05 VALUE "WHAT YOU WANT TO DO?"  LINE 11 COL 10.
+           05 VALUE "1. SEARCH ANOTHER STUDENT [S] OR [1] "  LINE 12
+               COL 10.
+           05 VALUE "2. ENTER PAYMENT [P] OR [2] " LINE 13
+               COL 10.
+           05 VALUE "3. GENERATE REPORT [R] OR [3] " LINE 14
+               COL 10.
+           05 VALUE "3. QUIT [Q] OR [4]" LINE 15 COL 10.
+           05 VALUE "ENTER THE OPTION: " LINE 16 COL 10.
+           05 OPTION-IN LINE 16 COL 27 PIC 9 TO WS-OPTION.
 
        PROCEDURE DIVISION.
        MAIN-PROCEDURE.
@@ -129,12 +175,20 @@
            PERFORM ACCEPT-USER-KEY.
            PERFORM INITIALIZE-FILE.
            PERFORM FIND-MATCH.
-           PERFORM OUTPUT-DISPLAY.
            PERFORM CLOSE-FILE.
            STOP RUN.
 
        ACCEPT-USER-KEY.
            ACCEPT INPUT-SCREEN.
+           IF WS-OPTION = 1 OR WS-OPTION = 'S' THEN
+               ACCEPT SEARCH-SCREEN
+               PERFORM OUTPUT-DISPLAY
+           ELSE IF WS-OPTION = 2 OR WS-OPTION = 'P' THEN
+               ACCEPT UPDATE-SCREEN
+               ACCEPT UPDATE-OUT-SCREEN
+           ELSE IF WS-OPTION = 4 OR WS-OPTION = 'R' THEN
+               ACCEPT INPUT-SCREEN
+           END-IF.
 
        INITIALIZE-FILE.
            OPEN INPUT STUDENT-RECORD-FILE.
